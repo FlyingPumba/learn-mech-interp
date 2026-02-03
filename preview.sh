717ea0f82f01f97d9ae4c1d28dad718aeb75d8ff
Ivan Arcuschin <iarcuschin@gmail.com>
2026-02-03 10:55:11 -0300

    chore: add preview script for local development

---
+++ preview.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting local development server..."
+echo ""
+echo "View the site at: http://localhost:8080/learn-mech-interp/"
+echo ""
+echo "Press Ctrl+C to stop the server"
+echo ""
+
+npm run start
